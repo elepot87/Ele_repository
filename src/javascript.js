15c8c7081916f8b0c11d24166ac081cef0b54249
elepot87 <78643839+elepot87@users.noreply.github.com>
2021-02-28 16:43:25 +0100

    use my set of icons

--- src/javascript.js
+++ src/javascript.js
@@ -57,19 +57,127 @@ function displayTemp(response) {
   speed.innerHTML = response.data.wind.speed;
   let currentPosition = document.querySelector("h1");
   currentPosition.innerHTML = response.data.name;
-  let iconElement = document.querySelector("#main-icon");
-  if (response.data.weather[0].main == `Clear`) {
-    iconElement.setAttribute("src", `images/sole_half.png`);
-  }
-  let iconElementMobile = document.querySelector("#main-icon-mobile");
-  if (response.data.weather[0].main == `Clear`) {
-    iconElementMobile.setAttribute("src", `images/sole_mobile.png`);
 
+  //Use custom icon for desktop
+  let iconElement = document.querySelector("#main-icon");
+  if (response.data.weather[0].description == `clear sky`) {
+    iconElement.setAttribute("src", `images/sole_half.png`);
     let suggestionElement = document.querySelector("#suggestion");
     suggestionElement.innerHTML = "The sun is shine!";
+  } else if (response.data.weather[0].description == `few clouds`) {
+    iconElement.setAttribute("src", `images/sole_nuvola_half.png`);
+    let suggestionElement = document.querySelector("#suggestion");
+    suggestionElement.innerHTML = "Not bad, but it could be better!";
+  } else if (
+    response.data.weather[0].description == `light snow` ||
+    response.data.weather[0].description == `Snow` ||
+    response.data.weather[0].description == `Heavy snow` ||
+    response.data.weather[0].description == `Sleet` ||
+    response.data.weather[0].description == `Light shower sleet` ||
+    response.data.weather[0].description == `Shower sleet` ||
+    response.data.weather[0].description == `Light rain and snow` ||
+    response.data.weather[0].description == `Rain and snow` ||
+    response.data.weather[0].description == `Light shower snow` ||
+    response.data.weather[0].description == `Shower snow` ||
+    response.data.weather[0].description == `Heavy shower snow`
+  ) {
+    iconElement.setAttribute("src", `images/fiocco_neve_half.png`);
+    let suggestionElement = document.querySelector("#suggestion");
+    suggestionElement.innerHTML = "Oh, it's cold! Get a jacket!";
+  } else if (response.data.weather[0].main == `Rain`) {
+    iconElement.setAttribute("src", `images/pioggia_half.png`);
+    let suggestionElement = document.querySelector("#suggestion");
+    suggestionElement.innerHTML = "You need an umbrella!";
+  } else if (response.data.weather[0].main == `Drizzle`) {
+    iconElement.setAttribute("src", `images/pioviggine_half.png`);
+    let suggestionElement = document.querySelector("#suggestion");
+    suggestionElement.innerHTML = "Oh, that's a boring weather!";
+  } else if (response.data.weather[0].main == `Thunderstorm`) {
+    iconElement.setAttribute("src", `images/temporale_half.png`);
+    let suggestionElement = document.querySelector("#suggestion");
+    suggestionElement.innerHTML = "It's better staying at home!";
+  } else if (
+    response.data.weather[0].main == `Mist` ||
+    response.data.weather[0].main == `Smoke` ||
+    response.data.weather[0].main == `Haze` ||
+    response.data.weather[0].main == `Dust` ||
+    response.data.weather[0].main == `Fog` ||
+    response.data.weather[0].main == `Squall` ||
+    response.data.weather[0].main == `Tornado`
+  ) {
+    iconElement.setAttribute("src", `images/nebbia_half.png`);
+    let suggestionElement = document.querySelector("#suggestion");
+    suggestionElement.innerHTML = "It seems to be in Sleepy Hollow!";
+  } else {
+    if (
+      response.data.weather[0].description == `overcast clouds` ||
+      response.data.weather[0].description == `broken clouds` ||
+      response.data.weather[0].description == `scattered clouds`
+    ) {
+      iconElement.setAttribute("src", `images/nuvola_half.png`);
+      let suggestionElement = document.querySelector("#suggestion");
+      suggestionElement.innerHTML = "Uff, where are you sun?";
+    }
+  }
 
+  //Use custom icon for mobile
+  let iconElementMobile = document.querySelector("#main-icon-mobile");
+  if (response.data.weather[0].description == `clear sky`) {
+    iconElementMobile.setAttribute("src", `images/sole_mobile.png`);
     let suggestionElementMobile = document.querySelector("#suggestion-mobile");
     suggestionElementMobile.innerHTML = "The sun is shine!";
+  } else if (response.data.weather[0].description == `few clouds`) {
+    iconElementMobile.setAttribute("src", `images/sole__nuvola_mobile.png`);
+    let suggestionElementMobile = document.querySelector("#suggestion-mobile");
+    suggestionElementMobile.innerHTML = "Not bad, but it could be better!";
+  } else if (
+    response.data.weather[0].description == `light snow` ||
+    response.data.weather[0].description == `Snow` ||
+    response.data.weather[0].description == `Heavy snow` ||
+    response.data.weather[0].description == `Sleet` ||
+    response.data.weather[0].description == `Light shower sleet` ||
+    response.data.weather[0].description == `Shower sleet` ||
+    response.data.weather[0].description == `Light rain and snow` ||
+    response.data.weather[0].description == `Rain and snow` ||
+    response.data.weather[0].description == `Light shower snow` ||
+    response.data.weather[0].description == `Shower snow` ||
+    response.data.weather[0].description == `Heavy shower snow`
+  ) {
+    iconElementMobile.setAttribute("src", `images/fiocco_neve.png`);
+    let suggestionElementMobile = document.querySelector("#suggestion-mobile");
+    suggestionElementMobile.innerHTML = "Oh, it's cold! Get a jacket!";
+  } else if (response.data.weather[0].main == `Rain`) {
+    iconElementMobile.setAttribute("src", `images/pioggia_mobile.png`);
+    let suggestionElementMobile = document.querySelector("#suggestion-mobile");
+    suggestionElementMobile.innerHTML = "You need an umbrella!";
+  } else if (response.data.weather[0].main == `Drizzle`) {
+    iconElementMobile.setAttribute("src", `images/pioviggine_mobile.png`);
+    let suggestionElementMobile = document.querySelector("#suggestion-mobile");
+    suggestionElementMobile.innerHTML = "Oh, that's a boring weather!";
+  } else if (
+    response.data.weather[0].main == `Mist` ||
+    response.data.weather[0].main == `Smoke` ||
+    response.data.weather[0].main == `Haze` ||
+    response.data.weather[0].main == `Dust` ||
+    response.data.weather[0].main == `Fog` ||
+    response.data.weather[0].main == `Squall` ||
+    response.data.weather[0].main == `Tornado`
+  ) {
+    iconElementMobile.setAttribute("src", `images/nebbia_mobile.png`);
+    let suggestionElementMobile = document.querySelector("#suggestion-mobile");
+    suggestionElementMobile.innerHTML = "It seems to be in Sleepy Hollow!";
+  } else {
+    if (
+      response.data.weather[0].description == `overcast clouds` ||
+      response.data.weather[0].description == `broken clouds` ||
+      response.data.weather[0].description == `scattered clouds`
+    ) {
+      iconElementMobile.setAttribute("src", `images/nuvola_mobile.png`);
+      let suggestionElementMobile = document.querySelector(
+        "#suggestion-mobile"
+      );
+      suggestionElementMobile.innerHTML = "Uff, where are you sun?";
+    }
   }
 }
 
